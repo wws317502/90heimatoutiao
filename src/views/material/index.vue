8032101ff93150b614940f6035bdd802f719cd93
yuminghui <2404446044@qq.com>
2019-12-23 21:55:42 +0800

    分页组件（1）

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -16,6 +16,7 @@
               </el-row>
             </el-card>
           </div>
+
         </el-tab-pane>
         <el-tab-pane label="收藏图片" name='collect'>
 
@@ -26,6 +27,16 @@
           </div>
         </el-tab-pane>
   </el-tabs>
+  <el-row type="flex" justify="center">
+            <el-pagination
+              background
+              layout="prev, pager, next"
+              :total="page.total"
+              :page-size="page.pageSize"
+              :current-page="page.currentPage"
+              @current-change="changepage"
+              ></el-pagination>
+          </el-row>
   </el-card>
 </template>
 
@@ -34,21 +45,35 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        currentPage: 1,
+        pageSize: 8,
+        total: 0
+
+      }
     }
   },
   methods: {
+    changepage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
     changeTab () {
+      this.page.currentPage = 1
       this.getMaterial()
     },
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
+          page: this.page.currentPage,
+          per_page: this.page.pageSize,
           collect: this.activeName === 'collect'
         }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
 
