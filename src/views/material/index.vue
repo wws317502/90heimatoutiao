86b2c678ae88c56863b1a5e436981bba609705ac
yuminghui <2404446044@qq.com>
2019-12-23 20:44:09 +0800

    素材列表（2）

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,11 +5,20 @@
       素材管理
     </template>
     </bread-crumb>
+    <el-tabs v-model="activeName">
+        <el-tab-pane label="全部图片" name='all'></el-tab-pane>
+        <el-tab-pane label="收藏图片" name='collect'></el-tab-pane>
+  </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 
 }
 </script>
