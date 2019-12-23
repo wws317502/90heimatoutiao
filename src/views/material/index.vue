05c0b803dc1f2f400a12ee1e3010dc1033215531
yuminghui <2404446044@qq.com>
2019-12-23 21:28:57 +0800

    数据加载（1）

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,9 +5,26 @@
       素材管理
     </template>
     </bread-crumb>
-    <el-tabs v-model="activeName">
-        <el-tab-pane label="全部图片" name='all'></el-tab-pane>
-        <el-tab-pane label="收藏图片" name='collect'></el-tab-pane>
+    <el-tabs v-model="activeName" @tab-click='changeTab'>
+        <el-tab-pane label="全部图片" name='all'>
+          <div class="img-list">
+            <el-card class="img-card" v-for="item in list" :key="item.id">
+              <img :src="item.url" alt="">
+              <el-row class="operate" type="flex" justify='space-around'>
+                <i class="el-icon-star-on"></i>
+                <i class="el-icon-delete-solid"></i>
+              </el-row>
+            </el-card>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="收藏图片" name='collect'>
+
+          <div class="img-list">
+            <el-card class="img-card" v-for="item in list" :key="item.id">
+              <img :src="item.url" alt="">
+            </el-card>
+          </div>
+        </el-tab-pane>
   </el-tabs>
   </el-card>
 </template>
@@ -16,13 +33,59 @@
 export default {
   data () {
     return {
-      activeName: 'all'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    changeTab () {
+      this.getMaterial()
+    },
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect'
+        }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+
+  },
+  created () {
+    this.getMaterial()
   }
 
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 220px;
+    margin: 20px 50px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      width: 100%;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      font-size: 20px;
+      height: 33px;
+      background-color: #f4f5f6;
+      i {
+        cursor: pointer;
+      }
+    }
+  }
+}
 
 </style>
