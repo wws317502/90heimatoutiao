39b57b252168245ceff52e92e75fab9192bd0c45
yuminghui <2404446044@qq.com>
2019-12-26 17:26:04 +0800

    分页请求（1）

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -56,11 +56,19 @@
               </div>
               <div class="right">
                   <span><i class="el-icon-edit"></i>修改</span>
-                  <span><i class="el-icon-delete"></i>删除</span>
-
+                  <span @click="dalMaterial(item.id)"><i class="el-icon-delete"></i>删除</span>
               </div>
-
           </div>
+          <el-row type='flex' justify='middle' style="height:60px">
+              <el-pagination
+                    background
+                    layout="prev, pager, next"
+                    :total="page.total"
+                    :current-page="page.currentPage"
+                    :page-size='page.pageSize'
+                    @current-change='changePage'
+                ></el-pagination>
+          </el-row>
   </el-card>
 </template>
 
@@ -75,7 +83,12 @@ export default {
       },
       channels: [],
       list: [],
-      defaultImg: require('../../assets/img/00.jpg')
+      defaultImg: require('../../assets/img/00.jpg'),
+      page: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   watch: {
@@ -123,8 +136,32 @@ export default {
   },
 
   methods: {
+    dalMaterial (id) {
+      this.$confirm('是否要删除文章').then(result => {
+        this.$axios({
+          method: 'delete',
+          url: `/articles/${id.toString()}`
+
+        }).then(result => {
+          this.$message({
+            type: 'success',
+            message: '删除成功'
+          })
+        })
+      })
+    },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getConditionArticles()
+    },
     changeCondition () {
+      this.page.currentPage = 1
+      this.getConditionArticles()
+    },
+    getConditionArticles () {
       let params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize,
         status: this.statusForm.status === 5 ? null : this.statusForm.status,
         channel_id: this.statusForm.channel_id,
         begin_pubdate: this.statusForm.dateRange.length ? this.statusForm.dateRange[0] : null,
@@ -138,6 +175,7 @@ export default {
         params
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     },
     getChannels () {
@@ -150,7 +188,7 @@ export default {
   },
   created () {
     this.getChannels()
-    this.getArticles()
+    this.getArticles({ page: 1, per_page: 10 })
   }
 
 }
