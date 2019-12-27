b9e1db7ea1a1a3f84c6e4db8e04d64afea63aaa7
yuminghui <2404446044@qq.com>
2019-12-27 09:15:36 +0800

    频道数据加载（1）

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -24,7 +24,14 @@
               </el-radio-group>
           </el-form-item>
           <el-form-item label='频道'>
-              <el-select></el-select>
+              <el-select>
+                  <el-option
+                  v-for="item in channels"
+                  :key="item.id"
+                  :label='item.name'
+                  :value='item.id'
+                  ></el-option>
+              </el-select>
           </el-form-item>
           <el-form-item>
               <el-button type='primary'>发布</el-button>
@@ -38,6 +45,23 @@
 
 <script>
 export default {
+  data () {
+    return {
+      channels: []
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 
 }
 </script>
